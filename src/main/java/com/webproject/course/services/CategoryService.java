ac913f2fa17737ae1fba801d4bc8529bec06875b
Luiz-021 <luizgute@outlook.com>
2025-03-30 12:55:00 -0300

    Category

---
+++ src/main/java/com/webproject/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.webproject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webproject.course.entities.Category;
+import com.webproject.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
